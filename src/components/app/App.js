ff19a38c7c8db0ef9b1fe4e1188236f756bcd78d
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2018-10-25 16:03:26 -0400

    Add webpack-dev-server.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const App = () => (
   <div>
-    Hello World
+    Hello World!
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devServer: {
+    contentBase: './dist',
+  },
   module: {
     rules: [
       {
